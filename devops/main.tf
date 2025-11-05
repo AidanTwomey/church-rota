dbf0daf1092b55fcf101409029a5e8c554cde68c
Aidan Twomey <aidantwomey@Aidans-Mac-mini.local>
2025-11-05 16:39:52 +0000

    add storage account

--- devops/main.tf
+++ devops/main.tf
@@ -32,16 +32,10 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "sql_admin_username" {
-  description = "SQL Server administrator username"
+variable "storage_account_name" {
+  description = "Name of the storage account (must be globally unique, 3-24 lowercase letters/numbers)"
   type        = string
-  default     = "sqladmin"
-}
-
-variable "sql_admin_password" {
-  description = "SQL Server administrator password"
-  type        = string
-  sensitive   = true
+  default     = "stchurchrota"
 }
 
 # Resource Group
@@ -56,98 +50,23 @@ resource "azurerm_resource_group" "main" {
   }
 }
 
-# App Service Plan
-resource "azurerm_service_plan" "main" {
-  name                = "asp-church-rota-${var.environment}"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  os_type             = "Linux"
-  sku_name            = "B1"
+# Storage Account
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  
+  min_tls_version          = "TLS1_2"
+  enable_https_traffic_only = true
 
   tags = {
     Environment = var.environment
     Project     = "ChurchRota"
   }
 }
-
-# App Service
-resource "azurerm_linux_web_app" "main" {
-  name                = "app-church-rota-${var.environment}"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  service_plan_id     = azurerm_service_plan.main.id
-
-  site_config {
-    always_on = false
-    
-    application_stack {
-      dotnet_version = "9.0"
-    }
-
-    cors {
-      allowed_origins = ["*"]
-    }
-  }
-
-  app_settings = {
-    "ASPNETCORE_ENVIRONMENT" = var.environment
-    "ConnectionStrings__DefaultConnection" = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.main.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${var.sql_admin_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-  }
-
-  https_only = true
-
-  tags = {
-    Environment = var.environment
-    Project     = "ChurchRota"
-  }
-}
-
-# SQL Server
-resource "azurerm_mssql_server" "main" {
-  name                         = "sql-church-rota-${var.environment}"
-  resource_group_name          = azurerm_resource_group.main.name
-  location                     = azurerm_resource_group.main.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
-  minimum_tls_version          = "1.2"
-
-  tags = {
-    Environment = var.environment
-    Project     = "ChurchRota"
-  }
-}
-
-# SQL Database
-resource "azurerm_mssql_database" "main" {
-  name           = "sqldb-church-rota-${var.environment}"
-  server_id      = azurerm_mssql_server.main.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  sku_name       = "Basic"
-  zone_redundant = false
-
-  tags = {
-    Environment = var.environment
-    Project     = "ChurchRota"
-  }
-}
-
-# Firewall rule to allow Azure services
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.main.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
-
-# Optional: Firewall rule to allow access from your IP (uncomment and set your IP)
-# resource "azurerm_mssql_firewall_rule" "allow_my_ip" {
-#   name             = "AllowMyIP"
-#   server_id        = azurerm_mssql_server.main.id
-#   start_ip_address = "YOUR_IP_ADDRESS"
-#   end_ip_address   = "YOUR_IP_ADDRESS"
-# }
 
 # Outputs
 output "resource_group_name" {
@@ -155,22 +74,19 @@ output "resource_group_name" {
   description = "Name of the resource group"
 }
 
-output "app_service_name" {
-  value       = azurerm_linux_web_app.main.name
-  description = "Name of the App Service"
+output "storage_account_name" {
+  value       = azurerm_storage_account.main.name
+  description = "Name of the storage account"
 }
 
-output "app_service_url" {
-  value       = "https://${azurerm_linux_web_app.main.default_hostname}"
-  description = "URL of the App Service"
+output "storage_account_primary_access_key" {
+  value       = azurerm_storage_account.main.primary_access_key
+  description = "Primary access key for the storage account"
+  sensitive   = true
 }
 
-output "sql_server_fqdn" {
-  value       = azurerm_mssql_server.main.fully_qualified_domain_name
-  description = "Fully qualified domain name of the SQL Server"
-}
-
-output "sql_database_name" {
-  value       = azurerm_mssql_database.main.name
-  description = "Name of the SQL Database"
+output "storage_account_primary_connection_string" {
+  value       = azurerm_storage_account.main.primary_connection_string
+  description = "Primary connection string for the storage account"
+  sensitive   = true
 }
