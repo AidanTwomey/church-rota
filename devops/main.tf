ae8fe587edd6f75bc4ac44a1b01bace9b9db4e63
Aidan Twomey <aidantwomey@Aidans-Mac-mini.local>
2025-11-06 18:54:17 +0000

    add function app to tf

--- devops/main.tf
+++ devops/main.tf
@@ -68,6 +68,73 @@ resource "azurerm_storage_account" "main" {
   }
 }
 
+# Storage Tables for Rota Data
+resource "azurerm_storage_table" "schedules" {
+  name                 = "Schedules"
+  storage_account_name = azurerm_storage_account.main.name
+}
+
+resource "azurerm_storage_table" "people" {
+  name                 = "People"
+  storage_account_name = azurerm_storage_account.main.name
+}
+
+resource "azurerm_storage_table" "availability" {
+  name                 = "Availability"
+  storage_account_name = azurerm_storage_account.main.name
+}
+
+# App Service Plan for Functions (Consumption/Serverless)
+resource "azurerm_service_plan" "function" {
+  name                = "asp-church-rota-function-${var.environment}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  os_type             = "Linux"
+  sku_name            = "Y1"  # Consumption plan (serverless/pay-per-use)
+
+  tags = {
+    Environment = var.environment
+    Project     = "ChurchRota"
+  }
+}
+
+# Azure Function App
+resource "azurerm_linux_function_app" "swap" {
+  name                       = "func-church-rota-swap-${var.environment}"
+  resource_group_name        = azurerm_resource_group.main.name
+  location                   = azurerm_resource_group.main.location
+  service_plan_id            = azurerm_service_plan.function.id
+  storage_account_name       = azurerm_storage_account.main.name
+  storage_account_access_key = azurerm_storage_account.main.primary_access_key
+
+  site_config {
+    application_stack {
+      dotnet_version              = "8.0"
+      use_dotnet_isolated_runtime = true
+    }
+
+    cors {
+      allowed_origins = ["*"]  # Update with your domain in production
+    }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"       = "dotnet-isolated"
+    "AzureWebJobsStorage"            = azurerm_storage_account.main.primary_connection_string
+    "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING" = azurerm_storage_account.main.primary_connection_string
+    "WEBSITE_CONTENTSHARE"           = "func-church-rota-swap-${var.environment}"
+    "TableStorageConnectionString"   = azurerm_storage_account.main.primary_connection_string
+    "ASPNETCORE_ENVIRONMENT"         = var.environment
+  }
+
+  https_only = true
+
+  tags = {
+    Environment = var.environment
+    Project     = "ChurchRota"
+  }
+}
+
 # Outputs
 output "resource_group_name" {
   value       = azurerm_resource_group.main.name
@@ -89,4 +156,19 @@ output "storage_account_primary_connection_string" {
   value       = azurerm_storage_account.main.primary_connection_string
   description = "Primary connection string for the storage account"
   sensitive   = true
+}
+
+output "function_app_name" {
+  value       = azurerm_linux_function_app.swap.name
+  description = "Name of the Function App"
+}
+
+output "function_app_url" {
+  value       = "https://${azurerm_linux_function_app.swap.default_hostname}"
+  description = "URL of the Function App"
+}
+
+output "function_app_id" {
+  value       = azurerm_linux_function_app.swap.id
+  description = "ID of the Function App"
 }
